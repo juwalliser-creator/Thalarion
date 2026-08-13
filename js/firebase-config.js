5c604ab890b493eb9542edc9b30b1af4caec7ba5
juwalliser-creator <juwalliser@proton.me>
2026-08-13 13:21:59 +0200

    änderung

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCxI6zRZAIOQ0lVAUczlHT_wiRTRDaKTZ4",
+  authDomain: "thalarion-69cd1.firebaseapp.com",
+  projectId: "thalarion-69cd1",
+  storageBucket: "thalarion-69cd1.firebasestorage.app",
+  messagingSenderId: "137124275627",
+  appId: "1:137124275627:web:1c8d0dbf4a37dd9dfe9eee"
+};
